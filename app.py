776c517cbd83795a1752acbc1342d7fdb6876a0b
Nil Vaghela <nilrajsinh9@icloud.com>
2023-11-03 19:32:43 -0400

    Version 1.0.0 Upload Live

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('Welcome.html')
+    return render_template('welcome.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
-
